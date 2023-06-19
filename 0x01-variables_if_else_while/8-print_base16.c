1aaa04e641e63635b2d3e55f2ab3ce82d17e6a3c
Punksnation <cyberpunk26@gmail.com>
2023-06-19 05:52:13 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+int num1;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+for (num1 = 'a'; num1 <= 'f'; num1++)
+{
+putchar(num1);
+}
+putchar('\n');
+return (0);
+}i
