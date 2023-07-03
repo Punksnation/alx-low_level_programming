2fabd0f7ac15865ef791d7d2c43869a21f5570db
Punksnation <cyberpunk26@gmail.com>
2023-07-03 10:17:02 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,20 @@
-#include "man.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- * _strspn - compute length of matching contiguous bytes
- * @s: string to calculate substring length from
- * @accept: string containing bytes to match
- * Return: length of matching contiguous bytes
- **/
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to a char, string that will be checked
+ * @accept: pointer to the substring we check for
+ * Return: unsigned int, length of a prefix substring
+*/
+
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int count = 0, x, i;
 
-unsigned int count = 0, match = 0, i;
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-															if (*s == accept[i])
-{
-count++;
-match = 1;
-															}															}
-if (!match)
-return (count);
-match = 0;
-s++;
-
-}
-return (count);
+	for (x = 0; s[x] != '\0' && x == count; x++)
+		for (i = 0; accept[i] != '\0'; i++)
+			if (s[x] == accept[i])
+				count++;
+	return (count);
 }
