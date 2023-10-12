dad32a07a068bd910006e07bb0d938292cb0467e
Punksnation <cyberpunk26@gmail.com>
2023-10-12 06:29:26 -0700

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,39 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-  * insert_dnodeint_at_index- insert at given index from a doubly linked list
-  * @h: start of doubly linked list
-  * @idx: index to insert value
-  * @n: value to insert
-  * Return: return the node that was inserted
-  */
+ * insert_dnodeint_at_index - inserts a new node in a list
+ * @h: head of the linked list
+ * @idx: index to insert the new node
+ * @n: int the new node should hold
+ * Return: pointer to the new node or NULL
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new, *hold = *h;
+	dlistint_t *prev, *new;
 
-	new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
 	new->n = n;
-
-	/* add to the beginning */
 	if (idx == 0)
 	{
 		new->next = *h;
 		new->prev = NULL;
-
-		if (*h)
-			(*h)->prev = new;
-
+		if (new->next)
+			new->next->prev = new;
 		*h = new;
-
 		return (new);
 	}
-
-	for (i = 0; hold; i++, hold = hold->next)
-		if (i == idx - 1)
-		{
-			new->next = hold->next;
-			if (new->next)
-				new->next->prev = new;
-			new->prev = hold;
-			hold->next = new;
-
-			return (new);
-		}
-	free(new);
+	for (prev = *h; prev && idx > 1; idx--)
+		prev = prev->next;
+	if (idx == 1)
+	{
+		new->next = prev->next;
+		new->prev = prev;
+		prev->next = new;
+		if (new->next)
+			new->next->prev = new;
+		return (new);
+	}
 	return (NULL);
 }
